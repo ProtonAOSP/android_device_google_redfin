3412fb6bcb3955ca2bccf24fb768296c88640c6e
Danny Lin <danny@kdrag0n.dev>
2020-11-03 11:28:04 -0800

    redfin: Remove stock product override name
    This is no longer necessary now that we've debranded the product name to
    match stock. Support for overriding the product name has been removed in
    the build system in favor of debranding device trees.
    
    Change-Id: Ibfdb9c8b1fc079fe18256e3d4922ea63253d5964

--- aosp_redfin.mk
+++ aosp_redfin.mk
@@ -64,6 +64,5 @@ PRODUCT_MODEL := Pixel 5
 
 # Match stock product info
 PRODUCT_OVERRIDE_INFO := true
-PRODUCT_OVERRIDE_NAME := redfin
 PRODUCT_OVERRIDE_FINGERPRINT := google/redfin/redfin:11/RD1A.200810.020/6792687:user/release-keys
 PRODUCT_OVERRIDE_DESC := redfin-user 11 RD1A.200810.020 6792687 release-keys
