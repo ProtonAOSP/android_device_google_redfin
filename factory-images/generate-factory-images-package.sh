4960f3cccadf42fcde50fc982ce2832b304345cd
Danny Lin <danny@kdrag0n.dev>
2021-03-01 22:52:12 -0800

    redfin: Update product info from RQ2A.210305.006
    Change-Id: Id04389c0cef1f2e6613931d4925eb621e68b559f

--- factory-images/generate-factory-images-package.sh
+++ factory-images/generate-factory-images-package.sh
@@ -18,8 +18,8 @@ source ../../../common/clear-factory-images-variables.sh
 BUILD=eng.$USER
 DEVICE=redfin
 PRODUCT=redfin
-VERSION=rq1c.210205.006
+VERSION=rq2a.210305.006
 SRCPREFIX=../../../../out/target/product/redfin/obj/PACKAGING/target_files_intermediates/
-BOOTLOADER=r3-0.3-6873951
-RADIO=g7250-00003-201212-B-7029028
+BOOTLOADER=r3-0.3-7051238
+RADIO=g7250-00069-210128-B-7108850
 source ../../../common/generate-factory-images-common.sh
