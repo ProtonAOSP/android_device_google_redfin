609b97deef0b2da3d32d2a546fc2d851bc7a5370
Danny Lin <danny@kdrag0n.dev>
2021-10-09 17:04:45 -0700

    Override build fingerprint from stock SPB5.210812.002
    Change-Id: I8b971dcefa3087ed1e81ec634199bc2c5a3ff27b

--- aosp_redfin.mk
+++ aosp_redfin.mk
@@ -75,3 +75,6 @@ PRODUCT_BRAND := google
 PRODUCT_NAME := aosp_redfin
 PRODUCT_DEVICE := redfin
 PRODUCT_MODEL := Pixel 5
+
+# Match stock build fingerprint
+PRODUCT_OVERRIDE_FINGERPRINT := google/redfin/redfin:12/SPB5.210812.002/7671067:user/release-keys
